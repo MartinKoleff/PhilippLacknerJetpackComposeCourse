5ad06f400237f232abddd3ee6436f97b4b9a5f6b
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-25 00:45:31 +0200

    Adding weigh scale lines calculation and drawing.

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/composable/Scale.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/composable/Scale.kt
@@ -11,7 +11,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
+import com.koleff.philipplacknerjetpackcomposecourse.model.LineType
 import com.koleff.philipplacknerjetpackcomposecourse.model.ScaleStyle
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.sin
 
 @Composable
 fun Scale(
@@ -31,6 +37,10 @@ fun Scale(
 
     var circleCenter by remember {
         mutableStateOf(Offset.Zero)
+    }
+
+    var angle by remember {
+        mutableStateOf(0f)
     }
 
     Canvas(modifier = modifier) {
@@ -60,6 +70,46 @@ fun Scale(
                         Color.argb(50, 0, 0, 0) //Black
                     )
                 }
+            )
+        }
+
+        //Drawing weight lines
+        for(i in minWeight..maxWeight){
+            val angleInRad = (i - initialWeight + angle - 90) * (PI / 180f).toFloat() //Polar coordinates
+
+            val lineType = when {
+                i % 10 == 0 -> LineType.TenStepLine
+                i % 5 == 0 -> LineType.FiveStepLine
+                else -> LineType.NormalLine
+            }
+
+            val lineLength = when (lineType){
+                LineType.NormalLine -> scaleStyle.normalLineLength.toPx()
+                LineType.FiveStepLine -> scaleStyle.fiveStepLineLength.toPx()
+                LineType.TenStepLine -> scaleStyle.tenStepLineLength.toPx()
+            }
+
+            val lineColor = when (lineType){
+                LineType.NormalLine -> scaleStyle.normalLineColor
+                LineType.FiveStepLine -> scaleStyle.fiveStepLineColor
+                LineType.TenStepLine -> scaleStyle.tenStepLineColor
+            }
+
+            val lineStart = Offset(
+                x = (outerRadius - lineLength) * cos(angleInRad) + circleCenter.x,
+                y = (outerRadius - lineLength) * sin(angleInRad) + circleCenter.y
+            )
+
+            val lineEnd = Offset(
+                x = outerRadius * cos(angleInRad) + circleCenter.x,
+                y = outerRadius * sin(angleInRad) + circleCenter.y
+            )
+
+            drawLine(
+                color = lineColor,
+                start = lineStart,
+                end = lineEnd,
+                strokeWidth = 1.dp.toPx()
             )
         }
     }
