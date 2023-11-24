9b98aa30cd79eab867fa957c8f747a49a2940f7b
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-24 23:23:08 +0200

    Completed custom text.

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.koleff.philipplacknerjetpackcomposecourse
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            
+            Canvas(modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)) {
+                drawContext.canvas.nativeCanvas.apply {
+                    drawText(
+                        "Test Text",
+                        100f,
+                        100f,
+                        Paint().apply {
+                            color = Color.RED
+                            textSize = 100f
+                        }
+                    )
+                }
+            }
         }
     }
 }
