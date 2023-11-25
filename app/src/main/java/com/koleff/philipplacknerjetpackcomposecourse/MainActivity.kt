5a39ee7a82148c42d479d00b36a60b72a4b6e57a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-26 01:20:20 +0200

    Added real time updater and displaying it with text.

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
@@ -1,14 +1,46 @@
 package com.koleff.philipplacknerjetpackcomposecourse
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import com.koleff.philipplacknerjetpackcomposecourse.composable.Clock
+import kotlinx.coroutines.delay
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : ComponentActivity() {
+
+    private val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+    private suspend fun startClock(): String {
+        delay(1000)
+        return dateTimeFormatter.format(LocalDateTime.now()) //To format as hh:mm:ss
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            
+            var currentTime by remember {
+                mutableStateOf(dateTimeFormatter.format(LocalDateTime.now()))
+            }
+
+            LaunchedEffect(key1 = currentTime){
+                currentTime = startClock()
+            }
+
+            Clock(modifier = Modifier.fillMaxSize(), currentTime = currentTime)
         }
     }
 }
--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/composable/Clock.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/composable/Clock.kt
@@ -3,6 +3,7 @@ package com.koleff.philipplacknerjetpackcomposecourse.composable
 import android.graphics.Color
 import android.graphics.Paint
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -16,16 +17,26 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.koleff.philipplacknerjetpackcomposecourse.model.ClockStyle
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Clock(
     modifier: Modifier,
-    currentTime: String = LocalDateTime.now(),
+    currentTime: String = dateTimeFormatter.format(LocalDateTime.now()),
     clockStyle: ClockStyle = ClockStyle()
 ) {
     var time by remember {
         mutableStateOf(currentTime)
     }
 
+    //When currentTime updates -> update time...
+    LaunchedEffect(key1 = currentTime) {
+        time = currentTime
+    }
 
     Canvas(modifier = modifier) {
 
@@ -45,6 +56,17 @@ fun Clock(
                         0f,
                         Color.argb(50, 0, 0, 0) //Black
                     )
+                }
+            )
+
+            drawText(
+                time,
+                center.x,
+                center.y + 200.dp.toPx(),
+                Paint().apply {
+                    color = Color.RED
+                    textSize = 20.sp.toPx()
+                    textAlign = Paint.Align.CENTER
                 }
             )
         }
