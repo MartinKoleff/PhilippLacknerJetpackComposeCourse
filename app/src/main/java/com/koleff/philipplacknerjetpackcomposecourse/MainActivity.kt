e127c6a739b209c148beb4f5d4e94aecd7423b4a
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-25 22:59:55 +0200

    Update MainActivity.kt

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
@@ -3,31 +3,13 @@ package com.koleff.philipplacknerjetpackcomposecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.koleff.philipplacknerjetpackcomposecourse.composable.Scale
+import com.koleff.philipplacknerjetpackcomposecourse.composable.WeightScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Scale(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(300.dp)
-                        .align(Alignment.BottomCenter)
-                ) {
-
-                }
-            }
+            WeightScale()
         }
     }
 }
