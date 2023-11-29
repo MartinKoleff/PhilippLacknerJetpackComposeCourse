bf9e1fc4c02732fe6b665592ae5226ce613e4bc1
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-29 16:50:04 +0200

    Expanded ClockStyle.

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/model/ClockStyle.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/model/ClockStyle.kt
@@ -6,7 +6,14 @@ import androidx.compose.ui.unit.dp
 
 data class ClockStyle(
     val clockRadius: Dp = 150.dp,
-    val hourArrowColor: Color = Color.Red,
-    val normalArrowColor: Color = Color.Black,
-    val arrowSize: Dp = 50.dp
+    val hoursArrowColor: Color = Color.LightGray,
+    val minutesArrowColor: Color = Color.Black,
+    val secondsArrowColor: Color = Color.Red,
+    val arrowLength: Dp = 50.dp,
+    val hourLineLength: Dp = 35.dp,
+    val minuteLineLength: Dp = 30.dp,
+    val minutesLineColor: Color = Color.Black,
+    val hoursLineColor: Color = Color.Green,
+    val totalHours: Int = 12,
+    val minuteLinesPerHour: Int = 5
 )
