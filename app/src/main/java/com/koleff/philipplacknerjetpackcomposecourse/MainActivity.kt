3471901758a1c720521624515e745b3690a116ec
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-29 16:51:34 +0200

    Added clock functions for testing.

--- app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/koleff/philipplacknerjetpackcomposecourse/MainActivity.kt
@@ -22,10 +22,9 @@ import java.time.format.DateTimeFormatter
 class MainActivity : ComponentActivity() {
 
     private val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-    private suspend fun startClock(): String {
-        delay(1000)
-        return dateTimeFormatter.format(LocalDateTime.now()) //To format as hh:mm:ss
-    }
+    private var hoursCounter = 15
+    private var minutesCounter = 30
+    private var secondsCounter = 45
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,12 +35,54 @@ class MainActivity : ComponentActivity() {
                 mutableStateOf(dateTimeFormatter.format(LocalDateTime.now()))
             }
 
-            LaunchedEffect(key1 = currentTime){
+            LaunchedEffect(key1 = currentTime) {
                 currentTime = startClock()
             }
 
             Clock(modifier = Modifier.fillMaxSize(), currentTime = currentTime)
         }
+    }
+
+    private suspend fun startClock(): String {
+        delay(1000)
+        return dateTimeFormatter.format(LocalDateTime.now()) //To format as hh:mm:ss
+    }
+
+    //Used for testing purposes
+    private suspend fun startClockHours(): String {
+        delay(1000)
+
+        if (hoursCounter + 1 > 24) {
+            hoursCounter = 0
+        } else {
+            hoursCounter++
+        }
+
+        return String.format("%d:%d:%d", hoursCounter, minutesCounter, secondsCounter)
+    }
+
+    private suspend fun startClockMinutes(): String {
+        delay(1000)
+
+        if (minutesCounter + 1 > 60) {
+            minutesCounter = 0
+        } else {
+            minutesCounter++
+        }
+
+        return String.format("%d:%d:%d", hoursCounter, minutesCounter, secondsCounter)
+    }
+
+    private suspend fun startClockSeconds(): String {
+        delay(1000)
+
+        if (secondsCounter + 1 > 60) {
+            secondsCounter = 0
+        } else {
+            secondsCounter++
+        }
+
+        return String.format("%d:%d:%d", hoursCounter, minutesCounter, secondsCounter)
     }
 }
 
